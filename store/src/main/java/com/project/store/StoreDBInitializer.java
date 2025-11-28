a9384cf8b120dd96d20a710cd371b2255be7ec05
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-11-27 20:54:45 -0800

    Update StoreDBInitializer.java
    consistent with mydb.sql

--- store/src/main/java/com/project/store/StoreDBInitializer.java
+++ store/src/main/java/com/project/store/StoreDBInitializer.java
@@ -19,14 +19,13 @@ public class StoreDBInitializer {
 
             stmt.executeUpdate("""
                 CREATE TABLE Customer (
-                    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+	                customer_id INT AUTO_INCREMENT PRIMARY KEY,
                     first_name VARCHAR(100) NOT NULL,
                     last_name VARCHAR(100) NOT NULL,
                     email VARCHAR(100) UNIQUE NOT NULL,
-                    password_hash VARCHAR(255) NOT NULL,
-                    phone VARCHAR(50),
-                    balance DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+                    address VARCHAR(255) NOT NULL,
+                    balance DECIMAL(10, 2) DEFAULT 0,
+                        password_hash VARCHAR(100) NOT NULL
                 );
             """);
 
@@ -34,18 +33,14 @@ public class StoreDBInitializer {
             stmt.executeUpdate("""
                 CREATE TABLE Product (
                     product_id INT AUTO_INCREMENT PRIMARY KEY,
-                    category_id INT NOT NULL,
-                    name VARCHAR(255) NOT NULL,
+	                name varchar(255) NOT NULL,
+	                category ENUM('Shirt', 'Blouse', 'Sweater', 'Hoodie', 'Jacket', 'Coat', 'Tank Top', 'Pants', 'Shorts', 'Skirt', 'Dress', 'Suit'),
                     description TEXT,
-                    brand VARCHAR(100),
-                    price DECIMAL(10,2) NOT NULL CHECK(price > 0),
-                    image_url VARCHAR(255),
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-
-                    FOREIGN KEY (category_id)
-                        REFERENCES Category(category_id)
-                        ON DELETE CASCADE
-                        ON UPDATE CASCADE
+                    brand varchar(100),
+                    price DECIMAL(10, 2) NOT NULL,
+                    sold boolean DEFAULT FALSE,
+        
+	                CHECK (price > 0)
                 );
             """);
 
@@ -53,17 +48,17 @@ public class StoreDBInitializer {
             stmt.executeUpdate("""
                 CREATE TABLE Product_Variant (
                     variant_id INT AUTO_INCREMENT PRIMARY KEY,
-                    product_id INT NOT NULL,
-                    size_id ENUM('XXS','XS','S','M','L','XL','XXL') NOT NULL,
-                    color_id ENUM('Black','White','Grey','Red','Blue','Green','Yellow') NOT NULL,
-                    stock INT NOT NULL DEFAULT 0 CHECK(stock >= 0),
-
-                    FOREIGN KEY (product_id)
-                        REFERENCES Product(product_id)
+                    product_id INT,
+	                color_id ENUM('Black', 'White', 'Grey', 'Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Purple', 'Pink'),		
+                    size_id ENUM('XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'),
+                    stock INT,
+    
+                    CONSTRAINT fk_product_product_variant
+		                FOREIGN KEY (product_id) REFERENCES Product(product_id)
                         ON DELETE CASCADE
                         ON UPDATE CASCADE,
-
-                    UNIQUE (product_id, size, color)
+    
+	                CHECK (stock >= 0)
                 );
             """);
 
@@ -72,39 +67,32 @@ public class StoreDBInitializer {
                     order_id INT AUTO_INCREMENT PRIMARY KEY,
                     customer_id INT NOT NULL,
                     variant_id INT NOT NULL,
-                    quantity INT NOT NULL CHECK(quantity > 0),
-                    total_price DECIMAL(10,2) NOT NULL CHECK(total_price >= 0),
-                    order_status ENUM('Pending','Paid','Shipped','Delivered','Cancelled')
-                    DEFAULT 'Pending',
-                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    quantity INT NOT NULL,
+                    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-                    FOREIGN KEY (customer_id)
-                        REFERENCES Customer(customer_id),
-
-                    FOREIGN KEY (variant_id)
-                        REFERENCES Product_Variant(variant_id)
+                    CONSTRAINT fk_customer_order
+                        FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                 );
             """);
 
 
             stmt.executeUpdate("""
                 CREATE TABLE Cart (
-                    customer_id INT NOT NULL,
+                    cart_id INT AUTO_INCREMENT PRIMARY KEY,
+	                customer_id INT NOT NULL,
                     variant_id INT NOT NULL,
-                    quantity INT NOT NULL DEFAULT 1 CHECK(quantity > 0),
-                    added_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-
-                    FOREIGN KEY (customer_id)
-                        REFERENCES Customer(customer_id)
-                        ON DELETE CASCADE
-                        ON UPDATE CASCADE,
-
-                    FOREIGN KEY (variant_id)
-                        REFERENCES Product_Variant(variant_id)
-                        ON DELETE CASCADE
-                        ON UPDATE CASCADE,
-
-                    UNIQUE (customer_id, variant_id)
+    
+                CONSTRAINT fk_customer_cart
+		            FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+        
+	            CONSTRAINT fk_variant_cart
+		            FOREIGN KEY (variant_id) REFERENCES Product_Variant(variant_id)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
                 );
             """);
 
