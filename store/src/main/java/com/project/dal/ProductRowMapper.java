d903e73b20f57b6a839f16f2c8ffdae3cc5838ba
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2025-12-01 21:17:17 -0800

    Update ProductRowMapper.java

--- store/src/main/java/com/project/dal/ProductRowMapper.java
+++ store/src/main/java/com/project/dal/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.project.dal;
 
+import com.project.enums.Category;
 import com.project.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,11 +15,11 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product p = new Product();
         p.setProductId(rs.getInt("product_id"));
         p.setName(rs.getString("name"));
-        //p.setCategory(rs.getString("category"));
+        p.setCategory(Category.fromDb(rs.getString("category")));
         p.setDescription(rs.getString("description"));
         p.setBrand(rs.getString("brand"));
         p.setPrice(rs.getBigDecimal("price"));
-        //p.setSold(rs.getBoolean("sold"));
+        p.setSold(rs.getBoolean("sold"));
 
         return p;
     }
