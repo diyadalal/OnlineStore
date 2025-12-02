63511ae505c7f9b04784641894b814a1963a0ee8
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-12-02 00:04:10 -0800

    Update Size.java

--- store/src/main/java/com/project/enums/Size.java
+++ store/src/main/java/com/project/enums/Size.java
@@ -2,4 +2,14 @@ package com.project.enums;
 
 public enum Size {
     XXS, XS, S, M, L, XL, XXL
+
+    //convert string to enum
+    public static Size fromDb(String dbValue) {
+        return Size.valueOf(dbValue.replace(" ", "_"));
+    }
+
+    //convert enum to string
+    public String toDb() {
+        return this.name().replace("_", " ");
+    }
 }
