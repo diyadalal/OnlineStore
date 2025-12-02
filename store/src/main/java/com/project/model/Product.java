374a5173e0460ea0188d371dcef22bbf2e04b133
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2025-12-01 21:13:55 -0800

    Update Product.java

--- store/src/main/java/com/project/model/Product.java
+++ store/src/main/java/com/project/model/Product.java
@@ -1,5 +1,7 @@
 package com.project.model;
 
+import com.project.enums.Category;
+
 import java.math.BigDecimal;
 
 public class Product {
@@ -8,15 +10,18 @@ public class Product {
     private String description;
     private String brand;
     private BigDecimal price;
+    private boolean sold;
+    private Category category;
 
     public Product() {}
 
-    public Product(int productId, String name, String description, String brand, BigDecimal price) {
+    public Product(int productId, String name, String description, String brand, BigDecimal price, boolean sold) {
         this.productId = productId;
         this.name = name;
         this.description = description;
         this.brand = brand;
         this.price = price;
+        this.sold = sold;
     }
 
     // Getters and Setters
@@ -30,4 +35,8 @@ public class Product {
     public void setBrand(String brand) { this.brand = brand; }
     public BigDecimal getPrice() { return price; }
     public void setPrice(BigDecimal price) { this.price = price; }
+    public void setSold(boolean sold) {  this.sold = true; }
+    public boolean isSold() { return sold; }
+    public void setCategory(Category category) { this.category = category; }
+    public Category getCategory() { return category; }
 }
