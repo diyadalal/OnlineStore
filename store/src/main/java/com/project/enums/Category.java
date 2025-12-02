261ffc32164b9acff41a1ddcebd0a8862a0f1b20
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2025-12-01 21:16:55 -0800

    Update Category.java

--- store/src/main/java/com/project/enums/Category.java
+++ store/src/main/java/com/project/enums/Category.java
@@ -1,5 +1,13 @@
 package com.project.enums;
 
 public enum Category {
-    Shirt, Blouse, Sweater, Hoodie, Jacket, Coat, Tank_Top, Pants, Shorts, Skirt, Dress, Suit
+    Shirt, Blouse, Sweater, Hoodie, Jacket, Coat, Tank_Top, Pants, Shorts, Skirt, Dress, Suit;
+
+    public static Category fromDb(String dbValue) {
+        return Category.valueOf(dbValue.replace(" ", "_"));
+    }
+
+    public String toDb() {
+        return this.name().replace("_", " ");
+    }
 }
