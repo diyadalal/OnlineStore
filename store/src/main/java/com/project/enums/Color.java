5bca90b25630f21d8a855c448c3b609dc4a4be42
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-12-02 00:03:24 -0800

    Update Color.java

--- store/src/main/java/com/project/enums/Color.java
+++ store/src/main/java/com/project/enums/Color.java
@@ -2,4 +2,13 @@ package com.project.enums;
 
 public enum Color {
     Black, White, Grey, Red, Blue, Green, Yellow, Orange, Purple, Pink
+    
+   //convert string to enum
+    public static Color fromDb(String dbValue) {
+        return Color.valueOf(dbValue.replace(" ", "_"));
+    }
+    //convert enum to string
+    public String toDb() {
+        return this.name().replace("_", " ");
+    }
 }
