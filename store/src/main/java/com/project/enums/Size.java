ce055415789a7907532d572edbb048faccb4e0b9
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2025-12-01 23:00:02 -0800

    Update Size.java

--- store/src/main/java/com/project/enums/Size.java
+++ store/src/main/java/com/project/enums/Size.java
@@ -1,5 +1,13 @@
 package com.project.enums;
 
 public enum Size {
-    XXS, XS, S, M, L, XL, XXL
+    XXS, XS, S, M, L, XL, XXL;
+
+    public static Size fromDb(String dbValue) {
+        return Size.valueOf(dbValue);
+    }
+
+    public String toDb() {
+        return this.name();
+    }
 }
