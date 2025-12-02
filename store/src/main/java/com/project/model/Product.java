a54432c9cee2a7a765b870816883d38425d33304
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-12-01 22:37:12 -0800

    Update Product.java
    added category and sold

--- store/src/main/java/com/project/model/Product.java
+++ store/src/main/java/com/project/model/Product.java
@@ -5,18 +5,22 @@ import java.math.BigDecimal;
 public class Product {
     private int productId;
     private String name;
+    private Category category;
     private String description;
     private String brand;
     private BigDecimal price;
+    private boolean sold;
 
     public Product() {}
 
-    public Product(int productId, String name, String description, String brand, BigDecimal price) {
+    public Product(int productId, String name, Category category, String description, String brand, BigDecimal price, boolean sold) {
         this.productId = productId;
         this.name = name;
+        this.category = category;
         this.description = description;
         this.brand = brand;
         this.price = price;
+        this.sold = sold;
     }
 
     // Getters and Setters
@@ -24,10 +28,14 @@ public class Product {
     public void setProductId(int productId) { this.productId = productId; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public Category getCategory() { return category; }
+    public Category setCategory() { this.category = category; }
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
     public String getBrand() { return brand; }
     public void setBrand(String brand) { this.brand = brand; }
     public BigDecimal getPrice() { return price; }
     public void setPrice(BigDecimal price) { this.price = price; }
+    public boolean isSold() { return sold; }
+    public void setSold(boolean sold) {this.sold = sold; }
 }
