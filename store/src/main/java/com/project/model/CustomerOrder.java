d09643865850b0aebabc9bc35856843cc7cfb928
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2025-12-01 23:13:01 -0800

    Update CustomerOrder.java

--- store/src/main/java/com/project/model/CustomerOrder.java
+++ store/src/main/java/com/project/model/CustomerOrder.java
@@ -1,36 +1,36 @@
 package com.project.model;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
-import com.project.enums.OrderStatus;
 
 public class CustomerOrder {
     private int orderId;
-    private int customerId; // Foreign Key
-    private int variantId;  // Foreign Key
-    private int quantity;
+    private int customerId;
+    private int variantId;
+    //private int quantity;
     private LocalDateTime orderDate;
 
     public CustomerOrder() {}
 
-    public CustomerOrder(int orderId, int customerId, int variantId, int quantity, BigDecimal totalPriceAtPurchase, String shippingAddressSnapshot, OrderStatus orderStatus, LocalDateTime orderDate) {
+    public CustomerOrder(int orderId, int customerId, int variantId, LocalDateTime orderDate) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.variantId = variantId;
-        this.quantity = quantity;
+        //this.quantity = quantity;
         this.orderDate = orderDate;
     }
 
-    // Getters and Setters
     public int getOrderId() { return orderId; }
     public void setOrderId(int orderId) { this.orderId = orderId; }
+
     public int getCustomerId() { return customerId; }
     public void setCustomerId(int customerId) { this.customerId = customerId; }
+
     public int getVariantId() { return variantId; }
     public void setVariantId(int variantId) { this.variantId = variantId; }
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    //public int getQuantity() { return quantity; }
+    //public void setQuantity(int quantity) { this.quantity = quantity; }
+
     public LocalDateTime getOrderDate() { return orderDate; }
     public void setOrderDate(LocalDateTime orderDate) { this.orderDate = orderDate; }
 }
