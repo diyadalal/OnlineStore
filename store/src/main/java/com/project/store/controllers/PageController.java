c1f96c36bce20ad7aac41f08c563da53a199cf1d
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-12-04 15:20:46 -0800

    Update PageController.java

--- store/src/main/java/com/project/store/controllers/PageController.java
+++ store/src/main/java/com/project/store/controllers/PageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-
+    //redirects user to the login page when they run the application
     @GetMapping("/")
     public String index() {
         return "redirect:/login";
