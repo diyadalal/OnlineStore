1954a8582228de1f3be2f07d2409dea20c5a7633
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-12-04 18:13:23 -0800

    Update ProductDAO.java
    adding filtering

--- store/src/main/java/com/project/dal/ProductDAO.java
+++ store/src/main/java/com/project/dal/ProductDAO.java
@@ -2,6 +2,7 @@ package com.project.dal;
 
 import com.project.model.Product;
 import java.util.List;
+import com.project.enums.Category;
 
 public interface ProductDAO {
     Product findById(int id);
@@ -9,4 +10,6 @@ public interface ProductDAO {
     boolean save(Product product);
     boolean update(Product product);
     boolean delete(int id);
+    List<Product> findByCategory(Category category);
 }
+
